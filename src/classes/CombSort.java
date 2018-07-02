e9e38be02104d7db0baec9da580ebd638781baf8
esoterickeys <sectorten@gmail.com>
2018-07-01 22:35:54 -0700

    Added Comb sorting algorithm.

---
+++ src/classes/CombSort.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) 2018 esoterickeys
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package classes;
+
+public class CombSort {
+
+    public CombSort() {
+
+    }
+
+    public int[] execute(int input[]) {
+        int winRatioNumerator = 30;
+        int winRatioDenominator = 10;
+
+        int window = (input.length / (winRatioNumerator / winRatioDenominator));
+
+        boolean exec = true;
+
+        while (exec) {
+            System.out.println("Current window: " + window);
+
+            int partialSort[] = input.clone();
+
+            for (int i = 0; i < input.length; i++) {
+                if (i + window > input.length - 1) {
+                    break;
+                } else if (input[i + window] < input[i]) {
+                    partialSort[i] = input[i + window];
+                    partialSort[i + window] = input[i];
+                }
+            }
+
+            input = partialSort.clone();
+            window--;
+
+            if (window < 1) {
+                exec = false;
+            }
+        }
+
+        return input;
+    }
+
+}
---
+++ test/classes/CombSortTest.java
@@ -0,0 +1,35 @@
+package classes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
+public class CombSortTest {
+
+    private int max = 2000;
+    private int size = 100;
+
+    @Test
+    public void test() {
+        Random rand = new Random();
+
+        int input[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            input[i] = rand.nextInt(max) - (max / 2);
+        }
+
+        CombSort ob = new CombSort();
+        int output[] = ob.execute(input);
+
+        System.out.println("Sorted Array");
+        for (int i = 0; i < size; i++) {
+            System.out.print(output[i] + " ");
+        }
+
+        for (int i = 0; i < size - 1; i++) {
+            Assert.assertTrue(output[i] <= output[i + 1]);
+        }
+    }
+}
